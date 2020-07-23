b1bcff45c958f107aaded68831bee87df404aa85
kdy7710 <64133364+kdy7710@users.noreply.github.com>
2020-07-23 18:24:24 +0900

    맴버관련 파일 생성

---
+++ arthall/src/main/java/member/MemberServiceImple.java
@@ -0,0 +1,10 @@
+package member;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MemberServiceImple implements MemberService{
+
+	
+	
+}
---
+++ arthall/src/main/java/member/MemberVO.java
@@ -0,0 +1,68 @@
+package member;
+
+import java.security.Timestamp;
+
+public class MemberVO {
+
+	private String memberNo;
+	private String id;
+	private String password;
+	private String name;
+	private String tell;
+	private String email;
+	private Timestamp birth;
+	private Timestamp regDate;
+	
+	public String getMemberNo() {
+		return memberNo;
+	}
+	public void setMemberNo(String memberNo) {
+		this.memberNo = memberNo;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getTell() {
+		return tell;
+	}
+	public void setTell(String tell) {
+		this.tell = tell;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Timestamp getBirth() {
+		return birth;
+	}
+	public void setBirth(Timestamp birth) {
+		this.birth = birth;
+	}
+	public Timestamp getRegDate() {
+		return regDate;
+	}
+	public void setRegDate(Timestamp regDate) {
+		this.regDate = regDate;
+	}
+	
+	
+	
+	
+}
